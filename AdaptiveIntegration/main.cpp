087abac9de312f712b08e65b7cadb729da53ed75
Daniel <daniel_kish@outlook.com>
2016-11-15 13:30:13 +0300

    Implemented quite efficient pool.

--- AdaptiveIntegration/main.cpp
+++ AdaptiveIntegration/main.cpp
@@ -15,6 +15,7 @@
 #include "threadsafe_queue.h"
 #include <sstream>
 #include <cassert>
+#include <random>
 
 using namespace std::chrono;
 using namespace std::chrono_literals;
@@ -23,7 +24,7 @@ struct waited_res {
 	bool ready;
 	double value;
 };
-
+enum State { childless, awaiting, used };
 struct Range
 {
 	Point a, b;
@@ -32,9 +33,11 @@ struct Range
 	waited_res left_child;
 	waited_res right_child;
 	waited_res *res;
+	State state;
 
 	Range(Point const& a_, Point const& b_, double eps_, waited_res* par)
-		: a{a_}, b{b_}, eps{eps_}, res{par}, left_child{false, -1.0}, right_child{false, 1.0}
+		: a{a_}, b{b_}, eps{eps_}, res{par}, left_child{false, -1.0}, right_child{false, 1.0},
+		state{childless}
 	{	}
 	Range()
 	{	}
@@ -71,28 +74,53 @@ std::map<std::thread::id, int> globalPops;
 std::map<std::thread::id, int> globalPushes;
 struct pendingParams { int min; int max; std::size_t times; std::size_t sum; };
 
+template <class T>
+void rightRotate(std::list<T>& l)
+{
+	std::list<T> tmp;
+	auto from = std::prev(l.end());
+	tmp.splice(tmp.begin(), l, from, l.end());
+	auto to = l.begin();
+	l.splice(to, tmp);
+}
+class Counter {
+	int cnt;
+	int limit;
+public:
+	Counter (int lim) : limit(lim)
+	{}
+	void operator++() {
+		cnt++;
+		if (cnt == limit)
+			cnt = 0;
+	}
+	operator int () {
+		return cnt;
+	}
+};
 
 struct Pool
 {
-	std::deque<Range> globalQ;
+	std::vector<Range> globalQ;
 	std::mutex globalMx;
 	std::atomic_bool done;
 	std::vector<std::thread> threads;
-	std::map<std::thread::id, pendingParams> pendingUsage;
 	int max_local_size;
 
-	Pool(int nth, int m=100) : done{false}, max_local_size{m}
+	using Pending = std::vector<Range>;
+	using iter = Pending::iterator;
+	static thread_local std::unique_ptr<Pending> pending;
+	static thread_local iter top;
+	static thread_local int idx;
+	static thread_local int doGlobalPush;
+	std::vector<bool> free;
+
+	Pool(int nth, int m = 2000) : done{false}, max_local_size{m}
 	{
+		free.resize(nth, true);
+		//globalQ.reserve(50);
 		for (int i = 0; i < nth; ++i)
-			threads.emplace_back(&Pool::worker, this);
-	}
-	void pendingCount(std::thread::id tid, std::deque<Range>& pending)
-	{
-		guard l(mio);
-		if (pending.size() > pendingUsage[tid].max)
-			pendingUsage[tid].max = pending.size();
-		pendingUsage[tid].sum += pending.size();
-		pendingUsage[tid].times++;
+			threads.emplace_back(&Pool::worker, this, i);
 	}
 	void shutdown()
 	{
@@ -106,7 +134,7 @@ struct Pool
 			return;
 		shutdown();
 	}
-	bool try_pop_global(Range& r)
+	bool globalPop(Range& r)
 	{
 		guard l(globalMx);
 		if (!globalQ.empty()) {
@@ -116,48 +144,131 @@ struct Pool
 		}
 		return false;
 	}
-	void worker()
+	std::tuple<double, double, Point> estimation(Range& r)
 	{
-		auto myid = std::this_thread::get_id();
-		std::deque<Range> pending;
-		while (!done)
-		{
-			while (!pending.empty() && pending.back().left_child.ready
-				&& pending.back().right_child.ready)
-			{
-				pending.back().res->value = pending.back().left_child.value + pending.back().right_child.value;
-				pending.back().res->ready = true;
-				pending.pop_back();
-			}
-			Range r;
-			if (!try_pop_global(r)) {
-				std::this_thread::yield();
-				continue;
-			}
-			const double s1 = trapezium_area(r.a, r.b);
-			const Point c = middle(r.a, r.b);
-			const double s2 = trapezium_area(r.a, c) + trapezium_area(c, r.b);
-			const double change = abs(s2 - s1);
+		const double s1 = trapezium_area(r.a, r.b);
+		const Point c = middle(r.a, r.b);
+		const double s2 = trapezium_area(r.a, c) + trapezium_area(c, r.b);
+		const double change = abs(s2 - s1);
+		return{change, s2, c};
+	}
+	bool both_ready(Range& r)
+	{
+		return r.left_child.ready && r.right_child.ready;
+	}
 
-			if (change < r.eps) {
-				r.res->value = s2;
-				r.res->ready = true;
-				continue;
-			}
-			pending.push_back(r);
-			{
-				guard l(globalMx);
-				globalQ.emplace_back(r.a, c, r.eps / 2, &pending.back().left_child);
-				globalQ.emplace_back(c, r.b, r.eps / 2, &pending.back().right_child);
+	bool find_free()
+	{
+		bool some_free{false};
+		for (int i = 0; i < free.size(); ++i)
+		{
+			if (i != idx && free[i]) {
+				some_free = true;
+				break;
 			}
 		}
+		return some_free;
+	}
+	void push(Range & r, Point const& c)
+	{
+		if (find_free() && doGlobalPush)
+		{
+			doGlobalPush--;
+			pending->emplace_back(r.a, c, r.eps / 2, &r.left_child);
+			guard l(globalMx);
+			globalQ.emplace_back(c, r.b, r.eps / 2, &r.right_child);
+		}
+		else {
+		pending->emplace_back(r.a, c, r.eps / 2, &r.left_child);
+		pending->emplace_back(c, r.b, r.eps / 2, &r.right_child);
+		}
+	}
+	void worker(int idx_)
+	{
+		idx = idx_;
+		auto myid = std::this_thread::get_id();
+		pending.reset(new Pending);
+		pending->reserve(10'000);
+		top = pending->begin();
+		doGlobalPush = 5000;
+		int max_s{0};
+		while (!done)
+		{
+			if (pending->empty()) free[idx] = true;
+			if (free[idx]) {
+				Range r;
+				if (globalPop(r)) {
+					pending->push_back(r);
+					free[idx] = false;
+					//{guard l(mio); std::cout << idx << " globalPop " << r.a.x() << ' ' << r.b.x() << '\n'; }
+				}
+				else {
+					free[idx] = true;
+					std::this_thread::yield();
+					continue;
+				}
+				top = pending->end() - 1;
+			}
+			if (pending->size() > max_s)
+				max_s = pending->size();
+			Range& r = *top;
+			if (r.state == childless)
+			{
+				double change, est; Point c;
+				std::tie(change, est, c) = estimation(r);
+				if (change < r.eps) {
+					r.res->value = est;
+					r.res->ready = true;
+					r.state = used;
+					continue;
+				}
+				r.state = awaiting;
+				push(r, c);
+				top = pending->end() - 1;
+			}
+			else if (both_ready(r) && r.state != used) {
+				r.res->value = r.left_child.value + r.right_child.value;
+				r.res->ready = true;
+				r.state = used;
+				continue;
+			}
+			else if (r.state == awaiting) {
+				--top;
+				if (top < pending->begin()) {
+					Range r;
+					if (globalPop(r)) pending->push_back(r);
+					top = pending->end() - 1;
+				}
+				continue;
+			}
+			else if (r.state == used) {
+				if (top == pending->end() - 1) { // 'used' on top -> delete
+					pending->pop_back();
+					top = pending->end() - 1;
+					continue;
+				}
+				--top;
+				if (top < pending->begin()) {
+					Range r;
+					if (globalPop(r)) pending->push_back(r);
+					top = pending->end() - 1;
+				}
+			}
+		}
+		//{guard l(mio); std::cout << pending->capacity() <<' '<< max_s <<' '<< doGlobalPush << '\n'; }
 	}
 	void submit(double a, double b, double eps, waited_res* promise)
 	{
 		Range r(a, b, eps, promise);
+		guard l(globalMx);
 		globalQ.push_back(r);
 	}
 };
+
+thread_local std::unique_ptr<Pool::Pending> Pool::pending;
+thread_local Pool::iter Pool::top;
+thread_local int Pool::idx;
+thread_local int Pool::doGlobalPush;
 
 double work(std::vector<chunk>& chunks, std::mutex& m_chunks)
 {
@@ -177,11 +288,9 @@ double work(std::vector<chunk>& chunks, std::mutex& m_chunks)
 			chunks.pop_back();
 		}
 		waited_res s{0.0,false};
-		int cnt{0};
 		tasks.emplace_back(c.a, c.b, c.eps, &s);
 		while (!tasks.empty())
 		{
-			cnt++;
 			if (tasks.back().left_child.ready && tasks.back().right_child.ready) {
 				tasks.back().res->ready = true;
 				tasks.back().res->value = tasks.back().left_child.value + tasks.back().right_child.value;
@@ -325,27 +434,28 @@ std::chrono::duration<double, std::milli> time_stats(Fun& f, int times = 10)
 		return durs[div];
 }
 
+
 int main()
 {
-	const double Eps = 1.0e-10;
-	double a = 1.0e-3, b = 1.0; double exact = 0.504066497877487; double S = 0.0;
-	
-	//Pool p(4);
-	//waited_res s{false, 0.0};
+	const double Eps = 1.0e-12;
+	double a = 0.01, b = 1.0; double exact = 0.503981893175415; double S = 0.0;
+
+	Pool p(4);
+	waited_res r{false, 0.0};
 	auto t1 = high_resolution_clock::now();
-	//p.submit(a, b, Eps, &s);
-	//while (!s.ready)
-	//	std::this_thread::yield();
-	//S = s.value;
-	
-	S = integralPar(a, b, Eps, 1, 50);
+
+	p.submit(a, b, Eps, &r);
+	while (!r.ready)
+		std::this_thread::yield();
+	//S = integralPar(a, b, Eps, 1, 1);
 	//S = integralR(a, b, Eps);
 	auto t2 = high_resolution_clock::now();
 	duration<double, std::milli> dur = (t2 - t1);
-	
-	std::cout << "ans = " << std::setprecision(-log10(Eps)) << std::fixed 
-		<< S <<' '<< abs(S-exact) << '\n';
-	std::cout << "elapsed " << dur.count() << " ms\n\n";
+	{ guard l(mio);
+	std::cout << "ans = " << std::setprecision(-log10(Eps)) << std::fixed
+		<< S << ' ' << abs(S - exact) << '\n';
+ 	std::cout << "elapsed " << dur.count  () << " ms\n\n";
+	}
 
 	//std::cout << "globalPops\n";
 	//for (auto& p : globalPops)
@@ -409,6 +519,21 @@ int main()
 //
 //for (auto p : usage)
 //	std::cout << p.first << ' ' << p.second << '\n';
+
+//Pool p(1);
+//int n = 1;
+//double dx = (b - a) / n;
+//std::vector<waited_res> results(n, {false,0.0});
+//for (int i = 0; i < n; ++i)
+//	p.submit(a + i*dx, a + (i + 1)*dx, Eps / n, &results[i]);
+//
+//for (int i = 0; i < n; ++i)
+//	while (!results[i].ready)
+//		std::this_thread::yield();
+//
+//for (auto& r : results)
+//S += r.value;
+
 
 //std::cout << "\nxs: " << xs.size() << '\n';
 //std::sort(begin(xs), end(xs));
